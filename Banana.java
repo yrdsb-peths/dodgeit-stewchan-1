3d79133df9d7fe35c4208aa0fb3c443c27ad00de
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2024-09-17 11:57:53 -0400

    Update banana

--- Banana.java
+++ Banana.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Banana here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The enemy
  */
 public class Banana extends Actor
 {
